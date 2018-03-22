c5f72b9b2b28acd5264395d0b34b9ffb6877e90b
Nathan Keolasy <keolazy1@gmail.com>
2018-03-22 14:11:55 -0500

    message

---
+++ expressServer.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+
+app.get('/pets', (req, res) => {
+  fs.readFile('./pets.json', 'utf8', (err, data) => {
+    res.send(JSON.parse(data))
+  })
+
+})
+
+app.listen(8000);
